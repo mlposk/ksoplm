773be000b664b53611324b093dfd2faf1306a96c
Alexander Molokotin <mlposk@yandex.ru>
2020-01-22 01:56:43 +0500

    add youtube video support

--- src/app/potd/potd.component.ts
+++ src/app/potd/potd.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './api.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-potd',
@@ -8,7 +9,10 @@ import { ApiService } from './api.service';
 })
 export class PotdComponent implements OnInit {
 
-  constructor(private apiService: ApiService) { }
+  constructor(
+    private apiService: ApiService,
+    private sanitizer: DomSanitizer
+  ) { }
   response;
   title = 'mlposk';
 
@@ -16,8 +20,29 @@ export class PotdComponent implements OnInit {
     this.apiService.getPic()
       .subscribe(data => {
         this.response = data;
+        if (this.response.media_type === 'video') {
+          const videoId = this.getYouTubeVideoId(this.response.url);
+          this.response.youtubeURL =
+            'https://youtube.com/embed/' +
+            videoId +
+            '?autoplay=1' +
+            '&loop=1' +
+            '&mute=1' +
+            '&showinfo=0' +
+            '&controls=0' +
+            '&playlist=' +
+            videoId;
+        }
         console.log(this.response);
       });
+  }
+
+  getYouTubeVideoId(url) {
+    return url.split('/')[4].split('?')[0];
+  }
+
+  sanitizeURL(url) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 
   ngOnInit() {
