e1394525f8e32f9b9e8428899b96ee2afef5a892
Alexander Molokotin <mlposk@yandex.ru>
2020-01-22 01:47:49 +0500

    add some spaces between braces :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PotdComponent } from './potd/potd.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
